dbe9a0d430e5ac83aefa19ac16063a96910cd8c8
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2014-09-13 01:34:38 -0400

    Basic styling, branched script tag to js file

---
+++ js/main.js
@@ -0,0 +1,103 @@
+function SimpleController($scope) {
+	$scope.aminos = [{
+		"structure" : "gylcine.svg",
+		"name" : "glycine",
+		"short" : "gly",
+		"code" : "G"
+	}, {
+		"structure" : "alanine.svg",
+		"name" : "alanine",
+		"short" : "ala",
+		"code" : "A"
+	}, {
+		"structure" : "valine.svg",
+		"name" : "valine",
+		"short" : "val",
+		"code" : "V"
+	}, {
+		"structure" : "leucine.svg",
+		"name" : "leucine",
+		"short" : "leu",
+		"code" : "L"
+	}, {
+		"structure" : "isoleucine.svg",
+		"name" : "isoleucine",
+		"short" : "ile",
+		"code" : "I"
+	}, {
+		"structure" : "serine.svg",
+		"name" : "serine",
+		"short" : "ser",
+		"code" : "S"
+	}, {
+		"structure" : "threonine.svg",
+		"name" : "threonine",
+		"short" : "thr",
+		"code" : "T"
+	}, {
+		"structure" : "phenylalanine.svg",
+		"name" : "phenylalanine",
+		"short" : "phe",
+		"code" : "F"
+	}, {
+		"structure" : "tyrosine.svg",
+		"name" : "tyrosine",
+		"short" : "tyr",
+		"code" : "Y"
+	}, {
+		"structure" : "tryptophan.svg",
+		"name" : "tryptophan",
+		"short" : "trp",
+		"code" : "W"
+	}, {
+		"structure" : "cysteine.svg",
+		"name" : "cysteine",
+		"short" : "cys",
+		"code" : "C"
+	}, {
+		"structure" : "methionine.svg",
+		"name" : "methionine",
+		"short" : "met",
+		"code" : "M"
+	}, {
+		"structure" : "aspartate.svg",
+		"name" : "aspartate",
+		"short" : "asp",
+		"code" : "D"
+	}, {
+		"structure" : "glutamate.svg",
+		"name" : "glutamate",
+		"short" : "glu",
+		"code" : "E"
+	}, {
+		"structure" : "asparagine.svg",
+		"name" : "asparagine",
+		"short" : "asn",
+		"code" : "N"
+	}, {
+		"structure" : "glutamine.svg",
+		"name" : "glutamine",
+		"short" : "gln",
+		"code" : "Q"
+	}, {
+		"structure" : "lysine.svg",
+		"name" : "lysine",
+		"short" : "lys",
+		"code" : "K"
+	}, {
+		"structure" : "arginine.svg",
+		"name" : "arginine",
+		"short" : "Arg",
+		"code" : "R"
+	}, {
+		"structure" : "histidine.svg",
+		"name" : "histidine",
+		"short" : "his",
+		"code" : "H"
+	}, {
+		"structure" : "proline.svg",
+		"name" : "proline",
+		"short" : "pro",
+		"code" : "P"
+	}];
+}
